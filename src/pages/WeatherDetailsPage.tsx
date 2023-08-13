ecdfe169d07d7d733bc85d2207b964487c569f6c
Zahra Mirali <z.mirali1997@gmail.com>
2023-08-13 19:09:09 +0200

    change details page

--- src/pages/WeatherDetailsPage.tsx
+++ src/pages/WeatherDetailsPage.tsx
@@ -1,23 +1,98 @@
-import React from "react";
-import { selectData } from "../store/weather/selectors";
-import { Space, Typography } from "antd";
+import React, { useState } from "react";
+import {
+  Layout,
+  Statistic,
+  Button,
+  Card,
+  Row,
+  Col,
+  Space,
+  Typography,
+  Divider,
+} from "antd";
 import { useSelector } from "react-redux";
-import WeatherList from "../components/WeatherList";
-import CurrentWeather from "../components/CurrentWeather";
+import { selectData } from "../store/weather/selectors";
 
 const WeatherDetailsPage: React.FC = () => {
   const weatherData = useSelector(selectData);
 
+  const data = weatherData.forecast.forecastday;
+
   return (
-    <Space direction="vertical">
-      <Typography>
-        {weatherData.location.name}, {weatherData.location.country} Detail
-      </Typography>
-      <CurrentWeather data={weatherData.forecast.forecastday[0]} />
-      <Space align="center">
-        <Typography>6 DAY FORECAST</Typography>
-      </Space>
-      <WeatherList data={weatherData.forecast.forecastday.slice(1)} />
+    <Space direction="vertical" style={{ display: "flex" }}>
+      {data.map(({ date, day }: any) => (
+        <Card>
+          <Typography>Day {date}</Typography>
+          <Divider />
+          <Row justify="space-between">
+            <img
+              width={72}
+              height={72}
+              alt={day.condition.text}
+              src={day.condition.icon}
+            />
+            <Statistic
+              title="avgtemp_c"
+              value={day.avgtemp_c}
+              prefix="°C"
+              style={{ marginBottom: 16 }}
+            />
+            <Space direction="vertical">
+              <Statistic
+                title="maxtemp_c"
+                value={day.maxtemp_c}
+                prefix="°C"
+                style={{ marginBottom: 16 }}
+              />
+              <Statistic
+                title="mintemp_c"
+                value={day.mintemp_c}
+                prefix="°C"
+                style={{ marginBottom: 16 }}
+              />
+            </Space>
+          </Row>
+          <p>{day.condition.text}</p>
+          <Row gutter={30}>
+            <Col xs={24} sm={12}>
+              <Row align="middle" justify="space-between">
+                <h3>Wind</h3>
+                <p>{day.maxwind_kph} km/h</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>UV Index</h3>
+                <p>{day.uv}</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>Humidity</h3>
+                <p>{day.avghumidity}</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>Visibility</h3>
+                <p>{day.avgvis_km} km</p>
+              </Row>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Row align="middle" justify="space-between">
+                <h3>Precipitation</h3>
+                <p>{day.totalprecip_mm} mm</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>Snow</h3>
+                <p>{day.totalsnow_cm} cm</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>Probability of Snow</h3>
+                <p>{day.daily_chance_of_snow}</p>
+              </Row>
+              <Row align="middle" justify="space-between">
+                <h3>Probability of Rain</h3>
+                <p>{day.daily_chance_of_rain}</p>
+              </Row>
+            </Col>
+          </Row>
+        </Card>
+      ))}
     </Space>
   );
 };
