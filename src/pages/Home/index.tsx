770adf0e293bb6a7187dc8fe604f0dc3ffa64d34
Zahra Mirali <z.mirali1997@gmail.com>
2023-08-14 10:46:36 +0200

    change props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,14 @@ const HomePage: React.FC = () => {
     <div>
       <CurrentWeather
         date={today.date}
-        feelslike_c={weatherData.current.feelslike_c}
-        pressure_mb={weatherData.current.pressure_mb}
+        feelslike={weatherData.current.feelslike_c}
+        pressure={weatherData.current.pressure_mb}
         conditionText={today.day.condition.text}
         conditionIcon={today.day.condition.icon}
-        avgtemp_c={today.day.avgtemp_c}
-        maxwind_kph={today.day.maxwind_kph}
-        avghumidity={today.day.avghumidity}
-        avgvis_km={today.day.avgvis_km}
+        avgTemp={today.day.avgtemp_c}
+        maxWind={today.day.maxwind_kph}
+        avgHumidity={today.day.avghumidity}
+        avgVis={today.day.avgvis_km}
       />
       <div className={styles.moreDetailBar}>
         <span className={styles.labelDaysForecast}>
